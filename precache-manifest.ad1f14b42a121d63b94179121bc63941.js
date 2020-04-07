3fe9f44147412d2ef1f070c2fbebd766d079ec68
talentbridger <talentbridger@gmail.com>
2020-04-06 17:12:10 -0700

    Updates

---
+++ precache-manifest.ad1f14b42a121d63b94179121bc63941.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d30b6d0c4a42281916791765389e07f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ed0471b0e30e6a68465",
+    "url": "/static/css/main.ceff88d2.chunk.css"
+  },
+  {
+    "revision": "d3c85e3e511608548289",
+    "url": "/static/js/2.1219f6ac.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/static/js/2.1219f6ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed0471b0e30e6a68465",
+    "url": "/static/js/main.6a6aaa88.chunk.js"
+  },
+  {
+    "revision": "6206e974cf4e6c56fc05",
+    "url": "/static/js/runtime-main.420ef8c1.js"
+  },
+  {
+    "revision": "459499e70ac896ea0fe13ac48fed411d",
+    "url": "/static/media/background.459499e7.jpg"
+  },
+  {
+    "revision": "72f28aa3b427a24d0eda039260314eb3",
+    "url": "/static/media/background2.72f28aa3.jpg"
+  }
+]);
